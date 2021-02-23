ca47ee521bcd7b5036ff6d888b6f52fe7ed2f2f4
RYAN SOHN <45253545+ryansohny@users.noreply.github.com>
2021-02-23 15:42:37 +0900

    Update and rename _03.tabix.sh to _03.bgzip_n_tabix.sh

--- SCNA/_03.bgzip_n_tabix.sh
+++ SCNA/_03.bgzip_n_tabix.sh
@@ -5,5 +5,6 @@ do
         sed -n ${i}p $1 > tmp${i}
         sample=$(awk '{print $1}' tmp${i})
         rm tmp${i}
+bgzip -@ 40 -c ${sample}.pileup > ${sample}.pileup.gz
 tabix -s 1 -b 2 -e 2 ${sample}.pileup.gz
 done
