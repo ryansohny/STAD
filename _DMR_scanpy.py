6b51485cd594bc826322d8938ddf9ea241d80a37
RYAN SOHN <45253545+ryansohny@users.noreply.github.com>
2022-01-13 02:44:59 +0900

    Update _DMR_scanpy.py

--- _DMR_scanpy.py
+++ _DMR_scanpy.py
@@ -34,6 +34,8 @@ sc.tl.leiden(dmr, resolution=1.0, key_added='leiden_r1')
 sc.tl.umap(dmr, min_dist=0.5, spread=1.0, n_components=2, alpha=1.0, gamma=1.0, init_pos='spectral', method='umap')
 sc.pl.umap(dmr, color='TN', add_outline=True, size=100, palette={'Normal':'Blue', 'Tumor':'Red'})
 
+
+
 # DMR table for tumor only
 dmr_t = sc.AnnData(pd.read_csv("DMR_mat_tumor_imputed.csv", index_col=0))
 dmr_t.raw = dmr
@@ -82,9 +84,28 @@ dmr_n_met.iloc[dmr_t_roworder, dmr_t_colorder]
 sns.heatmap(dmr_n_met.iloc[dmr_t_roworder, dmr_t_colorder], cmap=cmap, xticklabels=True)
 
 
+# RNA-seq
+rna = sc.AnnData(pd.read_csv("/data/Projects/phenomata/01.Projects/08.StomachCancer_backup/02.RNA-seq/STAD_SNUH_Tumor_leiden_vst.csv", index_col=0).T)
+rna.raw = rna
+rna.layers['vst'] = rna.X
+#rna.obs = dmr_t.obs # 이렇게 하면, rna.obs에 뭔가 update할 때마다 dmr_t.obs에도 그게 똑같이 들어감
+rna.obs = dmr_t.obs.copy()
+sc.pp.scale(rna)
+sc.tl.pca(rna, n_comps=83, zero_center=True)
+sc.pl.pca(rna, color='leiden_r1')
+# pca_variance_rna = pd.DataFrame(rna.uns['pca']['variance_ratio'], index=list(map(lambda x: 'PC' + str(x), list(range(1,84)))), columns=['Variance_ratio'])
+# np.sum(pca_variance_rna.values.flatten()[:25]) 
+
+sc.pp.neighbors(rna, n_neighbors=20, n_pcs=15)
+sc.tl.leiden(rna, resolution=0.5, key_added='rna_leiden_r05')
+sc.tl.leiden(rna, resolution=0.75, key_added='rna_leiden_r075')
+sc.tl.umap(rna, min_dist=0.5, spread=1.0, n_components=2, alpha=1.0, gamma=1.0, init_pos='spectral', method='umap')
+sc.pl.umap(rna, color=['leiden_r1', 'rna_leiden_r05', 'rna_leiden_r075'], add_outline=False)
+
 
 # https://github.com/aertslab/pySCENIC/issues/357 이거랑 (Vascular Aging)
 # https://doi.org/10.1016/j.celrep.2018.10.045 이거 archiving 해놓을것!! (Vascular Aging)
 # https://github.com/aertslab/pySCENIC/issues/136 이것도 archiving ==> pySCENIC on bulk RNA-seq DATA!!!!!!!!!!!!!
 # https://github.com/aertslab/pySCENIC/issues/169 이것도 archiving ==> multiple pySCENIC RUN?????? (Vascular Aging)
 # https://github.com/aertslab/pySCENIC/find/master 여기에 .ipynb 들 
+# UMAP 에 대한 영상인데, UMAP 만든 사람이 좋다고 함 https://www.youtube.com/watch?v=6BPl81wGGP8
