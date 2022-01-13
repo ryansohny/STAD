060b34d9398fa31f7bffb853d69d3b5dee267188
RYAN SOHN <45253545+ryansohny@users.noreply.github.com>
2022-01-13 10:33:36 +0900

    Update _DMR_scanpy.py

--- _DMR_scanpy.py
+++ _DMR_scanpy.py
@@ -10,7 +10,7 @@ plt.rcParams['figure.figsize'] = (5,5)
 cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["#104e8b", "#ffdab9", "#8b0a50"])
 %matplotlib
 
-# DMR table for ALL (tumor and normal)
+###### DMR table for ALL (tumor and normal)
 dmr = pd.read_csv("DMR_mat_imputed.csv", index_col=0).iloc[:,:-1]
 dmr = sc.AnnData(dmr)
 dmr.raw = dmr
@@ -36,11 +36,11 @@ sc.pl.umap(dmr, color='TN', add_outline=True, size=100, palette={'Normal':'Blue'
 
 
 
-# DMR table for tumor only
+###### DMR table for tumor only
 dmr_t = sc.AnnData(pd.read_csv("DMR_mat_tumor_imputed.csv", index_col=0))
 dmr_t.raw = dmr
 dmr_t.layers['Percent_met'] = dmr_t.X
-clinic_info = pd.read_csv('/data/Projects/phenomata/01.Projects/08.StomachCancer_backup/2021_WC300_clinical_information_Xadded.csv', index_col='ID')
+clinic_info = pd.read_csv('/data/Projects/phenomata/01.Projects/08.StomachCancer_backup/2021_WC300_clinical_information_Xadded_new.csv', index_col='ID')
 clinic_info[['T.stage', 'N.stage', 'M.stage', 'TNM.stage', 'HER2IHC']] = clinic_info[['T.stage', 'N.stage', 'M.stage', 'TNM.stage', 'HER2IHC']].astype(str)
 clinic_info = clinic_info.iloc[:,np.r_[0:17, -1]]
 dmr_t.obs = clinic_info
@@ -75,7 +75,7 @@ df = sc.get.obs_df(dmr_t, keys=['EpiBurden', 'leiden_r1'], use_raw=True)
 ax = sns.boxplot(x='leiden_r1', y='EpiBurden', data=df)
 ax = sns.stripplot(x='leiden_r1', y='EpiBurden', data=df, color=".3")
 
-# DMR table for normal only (for clustermap)
+###### DMR table for normal only (for clustermap)
 dmr_n = sc.AnnData(pd.read_csv("DMR_mat_normal_imputed.csv", index_col=0))
 dmr_n.raw = dmr_n
 dmr_n.layers['Percent_met'] = dmr_n.X
@@ -84,7 +84,7 @@ dmr_n_met.iloc[dmr_t_roworder, dmr_t_colorder]
 sns.heatmap(dmr_n_met.iloc[dmr_t_roworder, dmr_t_colorder], cmap=cmap, xticklabels=True)
 
 
-# RNA-seq
+###### RNA-seq
 rna = sc.AnnData(pd.read_csv("/data/Projects/phenomata/01.Projects/08.StomachCancer_backup/02.RNA-seq/STAD_SNUH_Tumor_leiden_vst.csv", index_col=0).T)
 rna.raw = rna
 rna.layers['vst'] = rna.X
@@ -103,9 +103,33 @@ sc.tl.umap(rna, min_dist=0.5, spread=1.0, n_components=2, alpha=1.0, gamma=1.0, 
 sc.pl.umap(rna, color=['leiden_r1', 'rna_leiden_r05', 'rna_leiden_r075'], add_outline=False)
 
 
+
+###### DMR table for tumor-normal
+dmr_tn = sc.AnnData(pd.read_csv("DMR_mat_tumor-normal_imputed.csv", index_col=0))
+dmr_tn.raw = dmr_tn
+dmr_tn.layers['met_diff'] = dmr_tn.X
+clinic_info = pd.read_csv('/data/Projects/phenomata/01.Projects/08.StomachCancer_backup/2021_WC300_clinical_information_Xadded_new.csv', index_col='ID')
+clinic_info[['T.stage', 'N.stage', 'M.stage', 'TNM.stage', 'HER2IHC']] = clinic_info[['T.stage', 'N.stage', 'M.stage', 'TNM.stage', 'HER2IHC']].astype(str)
+clinic_info = clinic_info.iloc[:,np.r_[0:17, -1]]
+dmr_tn.obs = clinic_info
+sc.pp.scale(dmr_tn)
+sc.tl.pca(dmr_tn, n_comps=83, zero_center=True)
+sc.pl.pca(dmr_tn, color='Lauren')
+# sc.pl.pca_variance_ratio(dmr_tn, log=True)
+
+#pca_variance_tn = pd.DataFrame(dmr_tn.uns['pca']['variance_ratio'], index=list(map(lambda x: 'PC' + str(x), list(range(1,84)))), columns=['Variance_ratio'])
+# np.sum(pca_variance_tn.values.flatten()[:12])
+
+sc.pp.neighbors(dmr_tn, n_neighbors=15, n_pcs=17)
+sc.tl.leiden(dmr_tn, resolution=1.0, key_added='tn_leiden_r1')
+sc.tl.umap(dmr_tn, min_dist=0.5, spread=1.0, n_components=2, alpha=1.0, gamma=1.0, init_pos='spectral', method='umap')
+sc.pl.umap(dmr_tn, color='tn_leiden_r1', add_outline=False)
+
+
 # https://github.com/aertslab/pySCENIC/issues/357 이거랑 (Vascular Aging)
 # https://doi.org/10.1016/j.celrep.2018.10.045 이거 archiving 해놓을것!! (Vascular Aging)
 # https://github.com/aertslab/pySCENIC/issues/136 이것도 archiving ==> pySCENIC on bulk RNA-seq DATA!!!!!!!!!!!!!
 # https://github.com/aertslab/pySCENIC/issues/169 이것도 archiving ==> multiple pySCENIC RUN?????? (Vascular Aging)
 # https://github.com/aertslab/pySCENIC/find/master 여기에 .ipynb 들 
 # UMAP 에 대한 영상인데, UMAP 만든 사람이 좋다고 함 https://www.youtube.com/watch?v=6BPl81wGGP8
+# sc.pp.highly_variable_genes 의 batch_key 항목 꼭 check!!!
