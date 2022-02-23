a53c2f1567f2d8dd51db7f26fc5a6d215bbb0c51
RYAN SOHN <45253545+ryansohny@users.noreply.github.com>
2022-02-24 00:04:54 +0900

    Update _Figures.py

--- _Figures.py
+++ _Figures.py
@@ -1,4 +1,17 @@
-# dfdfdf
+####################################################################################
+# Boxplot : Mean methylation (Tumor vs Normal)
 import seaborn as sns
-sns.boxplot(data=dmr_t.obs[['mean_met_normal', 'mean_met_tumor']], palette={'mean_met_normal': '#00008b','mean_met_tumor': '#8b0000'})
-sns.swarmplot(data=dmr_t.obs[['mean_met_normal', 'mean_met_tumor']], color=".2").set(ylabel='Mean Methylation Level')
+
+df = dmr_t.obs[['mean_met_normal', 'mean_met_tumor']]
+df.rename(columns={'mean_met_normal': 'Normal', 'mean_met_tumor': 'Tumor'}, inplace=True)
+
+sns.boxplot(data=df, palette={'Normal': '#00008b','Tumor': '#8b0000'})
+sns.swarmplot(data=df, color="k").set(ylabel='Mean Methylation Level')
+
+new_df = df['Tumor'] - df['Normal']
+sns.boxplot(data=new_df, color='lightsalmon')
+sns.swarmplot(data=new_df, color='k').set(ylabel='(Tumor - Normal) Mean methylation')
+
+stats.ttest_rel(df['Normal'], df['Tumor'])
+
+####################################################################################
