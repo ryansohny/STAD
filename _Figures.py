93f51d9fca3e3abdaa9ba821d119523602f33ca4
RYAN SOHN <45253545+ryansohny@users.noreply.github.com>
2022-03-15 19:33:17 +0900

    Update _Figures.py

--- _Figures.py
+++ _Figures.py
@@ -1,5 +1,6 @@
 ####################################################################################
 # Boxplot : Mean methylation (Tumor vs Normal)
+import pandas as pd
 import seaborn as sns
 
 df = dmr_t.obs[['mean_met_normal', 'mean_met_tumor']]
@@ -15,3 +16,7 @@ sns.swarmplot(data=new_df, color='k').set(ylabel='(Tumor - Normal) Mean methylat
 stats.ttest_rel(df['Normal'], df['Tumor'])
 
 ####################################################################################
+# Violinplot : PMD counts (Tumor vs Normal)
+df = pd.read_table("/data/Projects/phenomata/01.Projects/08.StomachCancer_backup/03.WGBS/PMD/PMD_counts.txt", sep='\t')
+sns.violinplot(data=df, palette={'Normal':'navy', 'Tumor':'darkred'}, cut=0, scale="count").set_xticklabels(['Normal', 'Tumor'])
+sns.stripplot(data=df, color="k").set_title('PMD counts')
