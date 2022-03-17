81ceb50623c08340bf392adea58653e515fece67
RYAN SOHN <45253545+ryansohny@users.noreply.github.com>
2022-03-17 15:17:45 +0900

    Update _Figures.py

--- _Figures.py
+++ _Figures.py
@@ -1,5 +1,5 @@
 ####################################################################################
-# Boxplot : Mean methylation (Tumor vs Normal)
+# Boxplot : Mean methylation (Tumor vs Normal) ==> deprecated
 import pandas as pd
 import seaborn as sns
 
@@ -15,8 +15,29 @@ sns.swarmplot(data=new_df, color='k').set(ylabel='(Tumor - Normal) Mean methylat
 
 stats.ttest_rel(df['Normal'], df['Tumor'])
 
+# Violinplot : Mean methylation (Tumor vs Normal) ==> recent version
+clinic_info = pd.read_csv('/data/Projects/phenomata/01.Projects/08.StomachCancer_backup/2022_WC300_clinical_information_Xadded.csv', index_col='Sample')
+p = sns.violinplot(data=clinic_info.iloc[84:][['PercentMet_COV5_Normal', 'PercentMet_COV5_Tumor']], palette={'PercentMet_COV5_Normal':'navy', 'PercentMet_COV5_Tumor':'darkred'}, cut=0, scale="count")
+p = sns.stripplot(data=clinic_info.iloc[84:][['PercentMet_COV5_Normal', 'PercentMet_COV5_Tumor']], color="black")
+p.set_xticklabels(['Normal', 'Tumor'])
+p.set_title('Average Methylation')
+
 ####################################################################################
 # Violinplot : PMD counts (Tumor vs Normal)
 df = pd.read_table("/data/Projects/phenomata/01.Projects/08.StomachCancer_backup/03.WGBS/PMD/PMD_counts.txt", sep='\t')
 sns.violinplot(data=df, palette={'Normal':'navy', 'Tumor':'darkred'}, cut=0, scale="count").set_xticklabels(['Normal', 'Tumor'])
 sns.stripplot(data=df, color="k").set_title('PMD counts')
+
+####################################################################################
+# Violinplot : DNMT (Tumor vs Normal)
+
+dnmt1 = pd.concat([pd.DataFrame(rna.loc[['DNMT1']].iloc[:, :84].T.values.flatten(), columns=['Normal']), pd.DataFrame(rna.loc[['DNMT1']].iloc[:, 84:].T.values.flatten(), columns=['Tumor'])], axis=1).set_index(rna.columns[84:])
+sns.violinplot(data=dnmt1, palette={'Normal':'navy', 'Tumor':'darkred'}, cut=0, scale="count").set_title('DNMT1')
+
+dnmt3a = pd.concat([pd.DataFrame(rna.loc[['DNMT3A']].iloc[:, :84].T.values.flatten(), columns=['Normal']), pd.DataFrame(rna.loc[['DNMT3A']].iloc[:, 84:].T.values.flatten(), columns=['Tumor'])], axis=1).set_index(rna.columns[84:])
+sns.violinplot(data=dnmt3a, palette={'Normal':'navy', 'Tumor':'darkred'}, cut=0, scale="count").set_title('DNMT3A')
+
+dnmt3b = pd.concat([pd.DataFrame(rna.loc[['DNMT3B']].iloc[:, :84].T.values.flatten(), columns=['Normal']), pd.DataFrame(rna.loc[['DNMT3B']].iloc[:, 84:].T.values.flatten(), columns=['Tumor'])], axis=1).set_index(rna.columns[84:])
+sns.violinplot(data=dnmt3b, palette={'Normal':'navy', 'Tumor':'darkred'}, cut=0, scale="count").set_title('DNMT3B')
+
+
