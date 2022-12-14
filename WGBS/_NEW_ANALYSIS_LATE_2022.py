831552acd315cf15831500a453e16cdfe8f858a7
ryansohny <phenomata@gmail.com>
2022-12-15 00:29:26 +0900

    asdf

--- WGBS/_NEW_ANALYSIS_LATE_2022.py
+++ WGBS/_NEW_ANALYSIS_LATE_2022.py
@@ -237,3 +237,178 @@ sc.tl.leiden(dmr_met_adata, resolution=0.75, key_added='leiden_r075')
 sc.tl.leiden(dmr_met_adata, resolution=0.5, key_added='leiden_r05')
 sc.tl.umap(dmr_met_adata, min_dist=0.5, spread=1.0, n_components=2, alpha=1.0, gamma=1.0, init_pos='spectral', method='umap')
 sc.pl.umap(dmr_met_adata, color=['leiden_r075', 'leiden_r05', 'EpiBurden'], add_outline=False, legend_loc='right margin', color_map=cmap)
+
+
+####################################################################################################################################
+# Promoter - Gene Expression
+# RNA expression processing
+## Combat-corrected gene-level VST
+gene_vst = pd.read_table("/data/Projects/phenomata/01.Projects/08.StomachCancer_backup/02.RNA-seq/GENCODE_V24/STAD_SNUH_vst_ComBat.txt", index_col=0, sep=' ')
+#gene_vst = pd.read_table("/home/mhryan/Workspace/02.Projects/02.WC300/02.RNA-seq/STAD_SNUH_vst_ComBat.txt", index_col=0, sep=' ')
+gene_vst.columns = list(map(lambda x: 'X'+x, gene_vst.columns))
+
+## DEG results from DESeq2
+deg_tn = pd.read_csv("/data/Projects/phenomata/01.Projects/08.StomachCancer_backup/02.RNA-seq/GENCODE_V24/Tumor.Normal.compare_ComBat.csv", index_col=0)
+#deg_tn = pd.read_csv("/home/mhryan/Workspace/02.Projects/02.WC300/02.RNA-seq/Tumor.Normal.compare_ComBat.csv", index_col=0)
+#deg_tn = deg_tn[deg_tn.index.isin(list(map(lambda x: x.split('/')[2], pls.index)))]
+
+deg_genes = deg_tn[(deg_tn['padj'] < 0.01) & (deg_tn['baseMean'] > 10)].index
+
+## Combat-corrected transcript-level counts
+#trans_combat = pd.read_table("/data/Projects/phenomata/01.Projects/08.StomachCancer_backup/02.RNA-seq/GENCODE_V24/STAD_SNUH_tx_combat_counts.txt", index_col=0, sep=' ')
+trans_combat = pd.read_table("/data/Projects/phenomata/01.Projects/08.StomachCancer_backup/02.RNA-seq/GENCODE_V24/STAD_SNUH_tx_combat_norm_counts.txt", index_col=0, sep=' ')
+#trans_combat = pd.read_table("/home/mhryan/Workspace/02.Projects/02.WC300/02.RNA-seq/STAD_SNUH_tx_combat_norm_counts.txt", index_col=0, sep=' ')
+trans_combat.columns = list(map(lambda x: 'X'+x, trans_combat.columns))
+
+
+## Combat-corrected transcript-level log2(counts+1)
+trans_combat_log2 = np.log2(trans_combat + 1)
+del trans_combat
+
+## ENSEMBL transcript ID to GeneID table
+tx2gene = pd.read_table("/data/Projects/phenomata/01.Projects/08.StomachCancer_backup/02.RNA-seq/GENCODE_V24/txdb_geneSymbol.txt")
+#tx2gene = pd.read_table("/home/mhryan/Workspace/02.Projects/02.WC300/02.RNA-seq/txdb_geneSymbol.txt")
+
+# Promoter methylation processing
+
+## PLS info
+pls_info = pd.read_table("PLS_annotated_table_full_new_non-redundant.txt", index_col=0)
+## PLS info with DMR
+plsdmr_info = pd.read_table("PLS_annotated_table_full_new_non-redundant_DMR_abs10_woPMD.txt", index_col=0)
+## NO-PLS info
+nopls_info = pd.read_table("No_PLS_annotated_table_full_new.txt", index_col=0) # Genes w/ multiple overlapped prpomoter don't exist
+## NO-PLS info with DMR
+noplsdmr_info = pd.read_table("No_PLS_annotated_table_full_new_DMR_abs10_woPMD.txt", index_col=0)
+
+## Gene-biotype used downstream
+used_info = ['protein_coding',
+             'lincRNA',
+             '3prime_overlapping_ncRNA',
+             'antisense',
+             'bidirectional_promoter_lncRNA',
+             'macro_lncRNA',
+             'non_coding',
+             'processed_transcript',
+             'sense_intronic',
+             'sense_overlapping']
+pls_info = pls_info[pls_info['Type'].isin(used_info)]
+nopls_info = nopls_info[nopls_info['Type'].isin(used_info)]
+nopls_info = nopls_info[~nopls_info['GeneID'].isin(pls_info['GeneID'])]
+
+plsdmr_info = plsdmr_info[plsdmr_info['Type'].isin(used_info)]
+noplsdmr_info = noplsdmr_info[noplsdmr_info['Type'].isin(used_info)]
+noplsdmr_info = noplsdmr_info[~noplsdmr_info['GeneID'].isin(plsdmr_info['GeneID'])]
+
+pls = pd.read_table("cCRE_PLS_smooth_modified.txt", index_col=0)
+nopls = pd.read_table("NO_PLS_smooth_modified.txt", index_col=0)
+pls.columns = list(map(lambda x: 'X'+x, pls.columns))
+nopls.columns = list(map(lambda x: 'X'+x, nopls.columns))
+pls = pls[pls.index.isin(list(map(lambda x:'/'.join(x.split('/')[:-1]), pls_info.index)))]
+nopls = nopls[nopls.index.isin(list(map(lambda x:'/'.join(x.split('/')[:-1]), nopls_info.index)))]
+
+## Merging PLS and No_PLS
+comb_pls = pd.concat([pls, nopls])
+comb_pls = comb_pls*100
+comb_pls_info = pd.concat([pls_info, nopls_info])
+comb_pls.index = comb_pls_info.index
+
+comb_plsdmr_info = pd.concat([plsdmr_info, noplsdmr_info])
+comb_plsdmr = comb_pls[comb_pls.index.isin(comb_plsdmr_info.index)]
+comb_plsdmr = comb_plsdmr
+comb_plsdmr_info = comb_plsdmr_info[comb_plsdmr_info.index.isin(comb_plsdmr.index)]
+
+del pls, nopls, pls_info, nopls_info
+
+comb_pls
+comb_pls_info
+comb_plsdmr
+comb_plsdmr_info
+
+a = pd.DataFrame(trans_combat_log2[trans_combat_log2.index.isin(list(comb_pls_info[comb_pls_info['CpGi'] == 'Yes']['ENSTID'].values))].iloc[:,:84].mean(axis=1), columns=['CpGi Promoter'])
+b = pd.DataFrame(trans_combat_log2[trans_combat_log2.index.isin(list(comb_pls_info[comb_pls_info['CpGi'] == 'na']['ENSTID'].values))].iloc[:,:84].mean(axis=1), columns=['Non-CpGi Promoter'])
+ab = pd.concat([a,b], axis=1)
+#p = sns.boxplot(data=ab, palette={'CpGi Promoter':'#00203FFF', 'Non-CpGi Promoter':'#ADEFD1FF'}, width=0.5, showfliers = False)
+p = sns.violinplot(data=ab, palette={'CpGi Promoter':'#00203FFF', 'Non-CpGi Promoter':'#ADEFD1FF'}, width=0.5, showfliers = False, scale="width", cut=0)
+p = sns.stripplot(data=ab, jitter=True, marker='o', color='black', size=1.5, alpha=0.1)
+p.set_ylabel("Gene expression")
+p.set_title("CpG islands present in Promoter or not")
+plt.tight_layout()
+sns.despine()
+stats.ttest_ind(a, b)
+
+
+## Transcript Expression Distribution according to Histone Modifications
+total1 = comb_plsdmr[comb_plsdmr.index.isin(comb_plsdmr_info[(comb_plsdmr_info['K4me3'] == 'Yes') & (comb_plsdmr_info['K27ac'] == 'Yes') & (comb_plsdmr_info['K27me3'] == 'Yes')].index)]
+total2 = comb_plsdmr[comb_plsdmr.index.isin(comb_plsdmr_info[(comb_plsdmr_info['K4me3'] == 'Yes') & (comb_plsdmr_info['K27ac'] == 'Yes') & (comb_plsdmr_info['K27me3'] == 'na')].index)]
+total3 = comb_plsdmr[comb_plsdmr.index.isin(comb_plsdmr_info[(comb_plsdmr_info['K4me3'] == 'Yes') & (comb_plsdmr_info['K27ac'] == 'na') & (comb_plsdmr_info['K27me3'] == 'Yes')].index)]
+total4 = comb_plsdmr[comb_plsdmr.index.isin(comb_plsdmr_info[(comb_plsdmr_info['K4me3'] == 'Yes') & (comb_plsdmr_info['K27ac'] == 'na') & (comb_plsdmr_info['K27me3'] == 'na')].index)]
+total5 = comb_plsdmr[comb_plsdmr.index.isin(comb_plsdmr_info[(comb_plsdmr_info['K4me3'] == 'na') & (comb_plsdmr_info['K27ac'] == 'Yes') & (comb_plsdmr_info['K27me3'] == 'na')].index)]
+total6 = comb_plsdmr[comb_plsdmr.index.isin(comb_plsdmr_info[(comb_plsdmr_info['K4me3'] == 'na') & (comb_plsdmr_info['K27ac'] == 'na') & (comb_plsdmr_info['K27me3'] == 'Yes')].index)]
+total7 = comb_plsdmr[comb_plsdmr.index.isin(comb_plsdmr_info[(comb_plsdmr_info['K4me3'] == 'na') & (comb_plsdmr_info['K27ac'] == 'na') & (comb_plsdmr_info['K27me3'] == 'na')].index)]
+
+# RNA (transcript) expression distribution plot version 1
+p = sns.histplot( trans_combat_log2.iloc[:, :84].sum(axis=1).div(84)[trans_combat_log2.iloc[:, :84].sum(axis=1).div(84).index.isin( list(set(comb_plsdmr_info[comb_plsdmr_info.index.isin(total1.index)]['ENSTID'].values)) )], kde=True, stat='count', color=sns.color_palette("Accent", 7)[0])
+p = sns.histplot( trans_combat_log2.iloc[:, :84].sum(axis=1).div(84)[trans_combat_log2.iloc[:, :84].sum(axis=1).div(84).index.isin( list(set(comb_plsdmr_info[comb_plsdmr_info.index.isin(total2.index)]['ENSTID'].values)) )], kde=True, stat='count', color=sns.color_palette("Accent", 7)[1])
+p = sns.histplot( trans_combat_log2.iloc[:, :84].sum(axis=1).div(84)[trans_combat_log2.iloc[:, :84].sum(axis=1).div(84).index.isin( list(set(comb_plsdmr_info[comb_plsdmr_info.index.isin(total3.index)]['ENSTID'].values)) )], kde=True, stat='count', color=sns.color_palette("Accent", 7)[2])
+p = sns.histplot( trans_combat_log2.iloc[:, :84].sum(axis=1).div(84)[trans_combat_log2.iloc[:, :84].sum(axis=1).div(84).index.isin( list(set(comb_plsdmr_info[comb_plsdmr_info.index.isin(total4.index)]['ENSTID'].values)) )], kde=True, stat='count', color=sns.color_palette("Accent", 7)[3])
+p = sns.histplot( trans_combat_log2.iloc[:, :84].sum(axis=1).div(84)[trans_combat_log2.iloc[:, :84].sum(axis=1).div(84).index.isin( list(set(comb_plsdmr_info[comb_plsdmr_info.index.isin(total5.index)]['ENSTID'].values)) )], kde=True, stat='count', color=sns.color_palette("Accent", 7)[4])
+p = sns.histplot( trans_combat_log2.iloc[:, :84].sum(axis=1).div(84)[trans_combat_log2.iloc[:, :84].sum(axis=1).div(84).index.isin( list(set(comb_plsdmr_info[comb_plsdmr_info.index.isin(total6.index)]['ENSTID'].values)) )], kde=True, stat='count', color=sns.color_palette("Accent", 7)[5])
+p = sns.histplot( trans_combat_log2.iloc[:, :84].sum(axis=1).div(84)[trans_combat_log2.iloc[:, :84].sum(axis=1).div(84).index.isin( list(set(comb_plsdmr_info[comb_plsdmr_info.index.isin(total7.index)]['ENSTID'].values)) )], kde=True, stat='count', color=sns.color_palette("Accent", 7)[6])
+p.set_xlabel("Mean Log2 normalized RNA read counts across normal samples")
+p.set_ylabel("Number of transcripts")
+sns.despine()
+plt.xlim((0, 20))
+#plt.xlim((0, 12))
+plt.tight_layout()
+from matplotlib.ticker import StrMethodFormatter
+import matplotlib.ticker as ticker
+p.yaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
+
+# RNA (transcript) expression distribution plot version 2 ==> This is it
+fig, ax = plt.subplots(1,1, figsize=(7, 7), constrained_layout=True)
+sns.kdeplot(trans_combat_log2.iloc[:, :84].sum(axis=1).div(84)[trans_combat_log2.iloc[:, :84].sum(axis=1).div(84).index.isin( list(set(comb_plsdmr_info[comb_plsdmr_info.index.isin(total1.index)]['ENSTID'].values)) )], color=sns.color_palette("Accent", 7)[0], fill=False, ax=ax)
+sns.kdeplot(trans_combat_log2.iloc[:, :84].sum(axis=1).div(84)[trans_combat_log2.iloc[:, :84].sum(axis=1).div(84).index.isin( list(set(comb_plsdmr_info[comb_plsdmr_info.index.isin(total2.index)]['ENSTID'].values)) )], color=sns.color_palette("Accent", 7)[1], fill=False, ax=ax)
+sns.kdeplot(trans_combat_log2.iloc[:, :84].sum(axis=1).div(84)[trans_combat_log2.iloc[:, :84].sum(axis=1).div(84).index.isin( list(set(comb_plsdmr_info[comb_plsdmr_info.index.isin(total3.index)]['ENSTID'].values)) )], color=sns.color_palette("Accent", 7)[2], fill=False, ax=ax)
+sns.kdeplot(trans_combat_log2.iloc[:, :84].sum(axis=1).div(84)[trans_combat_log2.iloc[:, :84].sum(axis=1).div(84).index.isin( list(set(comb_plsdmr_info[comb_plsdmr_info.index.isin(total4.index)]['ENSTID'].values)) )], color=sns.color_palette("Accent", 7)[3], fill=False, ax=ax)
+sns.kdeplot(trans_combat_log2.iloc[:, :84].sum(axis=1).div(84)[trans_combat_log2.iloc[:, :84].sum(axis=1).div(84).index.isin( list(set(comb_plsdmr_info[comb_plsdmr_info.index.isin(total5.index)]['ENSTID'].values)) )], color=sns.color_palette("Accent", 7)[4], fill=False, ax=ax)
+sns.kdeplot(trans_combat_log2.iloc[:, :84].sum(axis=1).div(84)[trans_combat_log2.iloc[:, :84].sum(axis=1).div(84).index.isin( list(set(comb_plsdmr_info[comb_plsdmr_info.index.isin(total6.index)]['ENSTID'].values)) )], color=sns.color_palette("Accent", 7)[5], fill=False, ax=ax)
+sns.kdeplot(trans_combat_log2.iloc[:, :84].sum(axis=1).div(84)[trans_combat_log2.iloc[:, :84].sum(axis=1).div(84).index.isin( list(set(comb_plsdmr_info[comb_plsdmr_info.index.isin(total7.index)]['ENSTID'].values)) )], color=sns.color_palette("Accent", 7)[6], fill=False, ax=ax)
+ax.set_xlim((0, 17.5))
+ax.set_xlabel("Mean Log2 normalized RNA read counts across normal samples")
+sns.despine(ax=ax)
+
+
+# Promoter methylation distribution plot version 1
+p = sns.histplot(total1.iloc[:, :84].mean(axis=1)*100, kde=True, stat='density', color=sns.color_palette("Accent", 7)[0])
+p = sns.histplot(total2.iloc[:, :84].mean(axis=1)*100, kde=True, stat='density', color=sns.color_palette("Accent", 7)[1])
+p = sns.histplot(total3.iloc[:, :84].mean(axis=1)*100, kde=True, stat='density', color=sns.color_palette("Accent", 7)[2])
+p = sns.histplot(total4.iloc[:, :84].mean(axis=1)*100, kde=True, stat='density', color=sns.color_palette("Accent", 7)[3])
+p = sns.histplot(total5.iloc[:, :84].mean(axis=1)*100, kde=True, stat='density', color=sns.color_palette("Accent", 7)[4])
+p = sns.histplot(total6.iloc[:, :84].mean(axis=1)*100, kde=True, stat='density', color=sns.color_palette("Accent", 7)[5])
+p = sns.histplot(total7.iloc[:, :84].mean(axis=1)*100, kde=True, stat='density', color=sns.color_palette("Accent", 7)[6])
+plt.xlim((0, 100))
+sns.despine()
+p.set_xlabel("Mean Promoter DNA Methylation (%) across normal samples")
+plt.tight_layout()
+
+#plt.xlim((0, 12))
+plt.tight_layout()
+from matplotlib.ticker import StrMethodFormatter
+import matplotlib.ticker as ticker
+p.yaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
+
+# Promoter methylation distribution plot version 2
+ax = plt.subplot(1,1,1)
+sns.kdeplot(total1.iloc[:, :84].mean(axis=1)*100, color=sns.color_palette("Accent", 7)[0], ax=ax)
+sns.kdeplot(total2.iloc[:, :84].mean(axis=1)*100, color=sns.color_palette("Accent", 7)[1], ax=ax)
+sns.kdeplot(total3.iloc[:, :84].mean(axis=1)*100, color=sns.color_palette("Accent", 7)[2], ax=ax)
+sns.kdeplot(total4.iloc[:, :84].mean(axis=1)*100, color=sns.color_palette("Accent", 7)[3], ax=ax)
+sns.kdeplot(total5.iloc[:, :84].mean(axis=1)*100, color=sns.color_palette("Accent", 7)[4], ax=ax)
+sns.kdeplot(total6.iloc[:, :84].mean(axis=1)*100, color=sns.color_palette("Accent", 7)[5], ax=ax)
+sns.kdeplot(total7.iloc[:, :84].mean(axis=1)*100, color=sns.color_palette("Accent", 7)[6], ax=ax)
+plt.xlim((0, 100))
+ax.set_xlabel("Mean Promoter DNA Methylation (%) across normal samples")
+sns.despine()
+plt.tight_layout()
+
+
